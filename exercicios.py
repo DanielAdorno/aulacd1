e5cdbd598ef859559157d3f6283b008387ff2cdf
Wildson Felipe <wildson.felipe@gmail.com>
2026-06-10 15:39:53 -0300

    Envio das atividades
    Envio das atividades até parte do Capítulo 5. Continuarei posteriormente para completar.

---
+++ exercicios.py
@@ -0,0 +1,67 @@
+#print('Olá Mundo!')
+# Exercício 2.1 Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.
+# 10 + 20 x 30
+# 41 / 30
+# (94 + 2) X 6 - 1
+
+#print(10 + 20 * 30)
+#print(41 / 30)
+#print((94 + 2) * 6 - 1)
+
+# Exercício 2.2 Digite a seguinte expressão no interpretador: 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
+#print(10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2)
+
+# Programa 2.1 - Primeiro programa com variáveis
+# a = 2 
+# b = 3
+# print(a + b)
+
+# nome = 'Wildson Felipe'
+# idade = '44 Anos'
+# print (f'Meu nome é {nome}, tenho {idade}, e faço curso de Ciência de Dados Escola do Futuro do Estado de Goiás')
+
+# a = 3
+# b = 5
+# equacao = (2*a) * (3*b)
+# print ("o Resultado da equação é:", equacao)
+
+# Daniel = 9
+# Alyce = 42
+# Wildson = 44
+# resultado = Daniel + Alyce + Wildson
+# print(f"A Somatória da idade de Daniel, Alyce e Wildson é", resultado)
+
+# salario = 1612
+# aumento = 20
+# salario_final = salario + (salario * aumento/100)
+# print (f"Você recebeu um salario de {salario} e teve um ajuste de {aumento}% o salário final é {salario_final}")
+
+#  PrograMa 2.2 - Cálculo de auMento de salário
+# salario = 1500
+# aumento = 1500 * 5 / 100
+# print(salario + (aumento))
+
+# #Exercício 2.3 Faça um programa que exiba seu nome na tela.
+# print('Wildson Felipe')
+
+# #Exercício 2.4 Escreva um programa que exiba o resultado de 2a x 3b, em que
+# #a vale 3 e b vale 5.
+# a = 3
+# b = 5
+# print((2 * a) * (3 * b))
+
+# #Exercício 2.5 Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela. 
+# a = 5
+# b = 10
+# c = 20
+# print(a + b + c)
+
+# #Exercício 2.6 Modifique o Programa 2.2, de forma que ele calcule um aumento de 15% para um salário de R$ 750. 
+# salario = 750
+# aumento = 750 * 15 / 100
+# print(salario + (aumento))
+
+# salario = 1612
+# aumento = 20
+# salario_final = salario + (salario * aumento/100)
+# print (f"Você recebeu um salario de {salario} e teve um ajuste de {aumento}% o salário final é R${salario_final}")
