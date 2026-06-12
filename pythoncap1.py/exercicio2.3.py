77bbac91331c28399c118f5d70fef6741177ce89
Jamila Queiroz <jamilafq@gmail.com>
2026-06-12 10:27:47 -0300

    exercicios cap 2 and 3

---
+++ pythoncap1.py/exercicio2.3.py
@@ -0,0 +1,42 @@
+# faça um programa que exiba seu nome na tela.
+
+# nome = input ("Jamila Fernandes")
+# print(nome)
+
+#2.4 escreva um programa que exiba o resultado de 2a *3b em que a vale 3 e b vale 5
+
+# a= 3
+# b =5
+# resultado= (2*a)*(3*b)
+# print (resultado)
+
+#2.5 Escreva um programa que calcule a soma de tres variaveis e imprima o resultado na tela.
+
+# a= 2
+# b= 5
+# c= 3
+# resultado = (a+b+c)
+# print (resultado)
+
+#2.6 modifique o programa 2.2 de forma que ele calcule um aumento de 15% para um saladrio de r$ 750
+
+salario = 750
+aumento = salario *0.15
+saladrioAument = (salario + aumento )
+print (aumento)
+print (saladrioAument)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pythoncap3.py/exercicio31.py/exercicio3.4-3.5-3.6.py
@@ -0,0 +1,22 @@
+#3.4 escreva uma espressão para determinar se uma pessoa deve ou não pagar imposto.  CONSIDERE QUE  PAGAM IMPOSTO PESSOAS CUJO SALÁRIO É MAIOR QUE  R$1.200,00
+# salario = float(input("Digite o salário: R$ "))
+# salario = 1.200 and True or 1.201 and False
+# print ("Não pargar imposto ! ")
+
+
+ 
+# salario1 = float(input("Digite o salário: R$ "))
+
+# salario2 = 1.200 and True
+# salario3 = 1.200 and False
+
+# print  (int (salario1 > salario2 "pagar imposto"))
+# print int(salario2 > salario3 "pagar imposto")
+
+# como fazer este exercicio sem usar if else ???????
+
+salario = float(input("Digite o salário: R$ "))
+if salario >1200 and True:
+    print ("pagar imposto!")
+else:
+    print ("nao pagar imposto")
